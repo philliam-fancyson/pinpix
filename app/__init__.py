124539ffe1e2dd744eb444c2ff48720261d6d492
Phillip Nguyen <philn0415@gmail.com>
2024-07-10 14:35:00 -0700

    setup config and user tweaks

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
-from .models import db, User
-from .api.user_routes import user_routes
-from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
+
+from .models import db, User
+
+from .api.user_routes import user_routes
+from .api.auth_routes import auth_routes
 
 app = Flask(__name__, static_folder='../react-vite/dist', static_url_path='/')
 
--- app/models/user.py
+++ app/models/user.py
@@ -11,8 +11,11 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable = False, server_default=db.func.now())
 
     @property
     def password(self):
@@ -29,5 +32,7 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name
         }
