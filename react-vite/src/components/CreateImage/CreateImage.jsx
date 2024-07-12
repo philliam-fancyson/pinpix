3cb3d400a4e5a4203ea69b54febee3a2c1f2e367
Phillip Nguyen <philn0415@gmail.com>
2024-07-11 20:22:47 -0700

    front-end setup

--- react-vite/src/components/CreateImage/CreateImage.jsx
+++ react-vite/src/components/CreateImage/CreateImage.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewImage } from "../../redux/image";
 
 
 function CreateImage() {
+    const navigate = useNavigate();
     const [image, setImage] = useState(null);
     const [imageLoading, setImageLoading] = useState(false);
 
@@ -9,14 +13,15 @@ function CreateImage() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const formData = new FormData();
+        console.log(formData)
         // TODO Change this to key data in backend
         formData.append("image", image);
         // aws uploads can be a bit slow—displaying
         // some sort of loading message is a good idea
         setImageLoading(true);
-        // TODO Change this to thunk
-        await dispatch(createPost(formData));
-        history.push("/");
+        // await dispatch(addNewImage(formData));
+        // navigate("/")
+        ;
     }
 
     return (
--- react-vite/src/redux/image.js
+++ react-vite/src/redux/image.js
@@ -1,14 +1,20 @@
 const GET_IMAGES = 'image/getImages'
+const ADD_IMAGE = 'image/addImage'
 
 const getImages = (images) => ({
     type: GET_IMAGES,
     images
 })
 
+const addImage = (image) => ({
+  type: ADD_IMAGE,
+  image
+})
+
 // * Thunks
 export const getLatestImages = () => async (dispatch) => {
     try {
-      const response = await fetch("/api/images/");
+      const response = await fetch(`/api/images/`);
       if (response.ok) {
         const images = await response.json();
         dispatch(getImages(images));
@@ -21,14 +27,40 @@ export const getLatestImages = () => async (dispatch) => {
     }
   };
 
+export const addNewImage = (image) => async (dispatch) => {
+  const response = await fetch(`/api/images/`, {
+    method: "POST",
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     "Content-Type": "application/json",
+  //   },
+    body: image
+  });
+
+  if (response.ok) {
+    const { resPost } = await response.json();
+    console.log(resPost)
+    dispatch(addImage(resPost));
+  } else {
+  throw new Error("failed to add image");
+}
+}
+
 
 // * Reducer
-const initialState = { images: [] }
+const initialState = { images: [], image: {} }
 const imageReducer = (state = initialState, action) => {
-    // let newState = {}
+    let newState = {}
     switch(action.type) {
         case GET_IMAGES:
             return { ...state, images: action.images}
+          case ADD_IMAGE:
+            newState = {
+              ...state,
+              images: [...state.images, action.image],
+              image: action.image
+            }
+            return newState
         default:
             return state
     }
