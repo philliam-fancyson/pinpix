456311564d5e6c5e687b032f056ffde372bf25c1
Phillip Nguyen <philn0415@gmail.com>
2024-07-18 10:39:43 -0700

    css

--- react-vite/src/components/Main/Main.jsx
+++ react-vite/src/components/Main/Main.jsx
@@ -10,6 +10,7 @@ function Main() {
     const dispatch = useDispatch();
     const latestImages = useSelector(state => state.image.images)
     const user = useSelector(state => state.session.user);
+    const changeType = "a"
 
     useEffect(() => {
         dispatch(getLatestImages())
@@ -28,12 +29,26 @@ function Main() {
     return (
         <>
             <div className="landing-grid">
-                {latestImages && latestImages.map((image, index) =>
-                    <div key={index}>
+            {latestImages && latestImages.map((image, index) =>
+                (index + 1) % 3 === 0 ? (
+                    <div className="pin-card card-large" key={index}>
+                    <Link to={`/pin/${image.id}`}>
+                        <img src={image.image_url} />
+                    </Link>
+                    </div>
+                ) : (index + 1) % 2 === 0 ? (
+                    <div className="pin-card card-medium" key={index}>
                         <Link to={`/pin/${image.id}`}>
                             <img src={image.image_url} />
                         </Link>
-                    </div>
+                        </div>
+                ) : (
+                        <div className="pin-card card-small" key={index}>
+                        <Link to={`/pin/${image.id}`}>
+                            <img src={image.image_url} />
+                        </Link>
+                        </div>
+                )
                 )}
             </div>
         </>
@@ -41,3 +56,23 @@ function Main() {
 }
 
 export default Main;
+
+// if (index % 3 === 0) {
+//     <div className="pin-card card-large" key={index}>
+//     <Link to={`/pin/${image.id}`}>
+//         <img src={image.image_url} />
+//     </Link>
+// // </div>
+// } else if (index % 2 ===0) {
+    // <div className="pin-card card-medium" key={index}>
+    // <Link to={`/pin/${image.id}`}>
+    //     <img src={image.image_url} />
+    // </Link>
+// </div>
+// } else {
+//     <div className="pin-card card-small" key={index}>
+//     <Link to={`/pin/${image.id}`}>
+//         <img src={image.image_url} />
+//     </Link>
+//     </div>
+// }
